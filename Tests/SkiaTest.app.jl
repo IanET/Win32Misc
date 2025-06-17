065241703e4778cb72fbb0c2912adb7c96adc45f
IanET <1154506+IanET@users.noreply.github.com>
2025-06-17 15:19:33 -0700

    ...

--- Tests/SkiaTest.app.jl
+++ Tests/SkiaTest.app.jl
@@ -20,12 +20,15 @@ const MIN_HEIGHT = 200
 const BLUE_GRAY_BRUSH = CreateSolidBrush(RGB(0xD0, 0xD0, 0xE0))
 const HINST::HINSTANCE = GetModuleHandleW(C_NULL)
 
+# Helpers
 cwstring(s) = cconvert(Cwstring, s)
 tostring(v::AbstractArray{Cwchar_t}) = transcode(String, @view v[begin:findfirst(iszero, v)-1])
 tolparam(s::String) = s |> cwstring |> pointer |> LPARAM
 Base.Tuple(v::MemoryRef{UInt16}, len) = copyto!(zeros(eltype(v), len), v.mem) |> Tuple
 size_t(i::Int64) = reinterpret(UInt64, i) |> SIZE_T
 flush_ws() = SetProcessWorkingSetSize(GetCurrentProcess(), size_t(-1), size_t(-1))
+Base.fieldoffset(T::Type, field::Symbol) = fieldoffset(T, Base.fieldindex(T, field))
+unsafe_modify_cstruct(pstruct::Ptr{T}, field::Symbol, newval::V) where {T, V} = unsafe_store!(Ptr{V}(pstruct + fieldoffset(T, field)), newval)
 
 _layout = GridLayout( 
     [   
@@ -192,13 +195,8 @@ function onCommand(hwnd, id, code)
     return 0
 end
 
-# Modify a c-struct in place
-Base.fieldoffset(T::Type, field::Symbol) = fieldoffset(T, Base.fieldindex(T, field))
-unsafe_modify_cstruct(pstruct::Ptr{T}, field::Symbol, newval::V) where {T, V} = unsafe_store!(Ptr{V}(pstruct + fieldoffset(T, field)), newval)
-
 function onGetMinMaxInfo(hwnd, pmmi::Ptr{MINMAXINFO})
     unsafe_modify_cstruct(pmmi, :ptMinTrackSize, POINT(MIN_WIDTH, MIN_HEIGHT))
-    # @info unsafe_load(pmmi)
     return 0
 end
 
