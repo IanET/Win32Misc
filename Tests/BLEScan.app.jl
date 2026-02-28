bf09d6bab7539f86a4c76fe853dce81ca325e9fc
IanET <1154506+IanET@users.noreply.github.com>
2026-02-28 09:48:16 -0800

    Use generic event handler

--- Tests/BLEScan.app.jl
+++ Tests/BLEScan.app.jl
@@ -4,7 +4,6 @@ include("../common/Win32.jl")
 include("../common/combase.jl")
 using LibBaseTsd, Printf, .W32 
 
-# {AF86E2E0-B12D-4C6A-9C5A-D7AA65101E90}
 IID_Inspectable = GUID(0xaf86e2e0, 0xb12d, 0x4c6a, 0x9c5a, 0xd7aa65101e90)
 IID_IUriFactory = GUID(0x44a9796c, 0x1108, 0x4541, 0xa279, 0x042f0bd441f1)
 IID_IUNKNOWN = GUID(0x00000000, 0x0000, 0x0000, 0xc000, 0x000000000046)
@@ -38,14 +37,6 @@ end
     Active = 1
 end
 
-@kwdef mutable struct IRecievedCallbackVtbl <: Vtbl
-    QueryInterface::Ptr{Cvoid} = C_NULL
-    AddRef::Ptr{Cvoid} = C_NULL
-    Release::Ptr{Cvoid} = C_NULL
-    Invoke::Ptr{Cvoid} = C_NULL
-end
-const IRecievedCallback = Interface{IRecievedCallbackVtbl}
-
 @interface IBluetoothLEAdvertisementWatcher begin
     @inherit IInspectable
     get_MinSamplingInterval::Ptr{Cvoid}
@@ -61,7 +52,7 @@ const IRecievedCallback = Interface{IRecievedCallbackVtbl}
     put_AdvertisementFilter(this::Ptr{IBluetoothLEAdvertisementWatcher}, value::Ptr{IBluetoothLEAdvertisementFilter})::HRESULT
     Start(this::Ptr{IBluetoothLEAdvertisementWatcher})::HRESULT
     Stop(this::Ptr{IBluetoothLEAdvertisementWatcher})::HRESULT
-    add_Received(this::Ptr{IBluetoothLEAdvertisementWatcher}, handler::Ptr{IRecievedCallback}, token::Ptr{UInt64})::HRESULT
+    add_Received(this::Ptr{IBluetoothLEAdvertisementWatcher}, handler::Ptr{IEventHandler}, token::Ptr{UInt64})::HRESULT
     remove_Received(this::Ptr{IBluetoothLEAdvertisementWatcher}, token::UInt64)::HRESULT
     add_Stopped::Ptr{Cvoid}
     remove_Stopped::Ptr{Cvoid}
@@ -122,7 +113,7 @@ filter = Ptr{IBluetoothLEAdvertisementFilter}(C_NULL) |> Ref
 get_AdvertisementFilter(watcher[], filter) |> AssertSuccess
 @info "Filter: $(filter[])"
 
-function RecievedCallback_QueryInterface(this::Ptr{IRecievedCallback}, riid::Ptr{GUID}, ppv::Ptr{Ptr{Cvoid}})::HRESULT
+function RecievedCallback_QueryInterface(this::Ptr{IEventHandler}, riid::Ptr{GUID}, ppv::Ptr{Ptr{Cvoid}})::HRESULT
     guid = unsafe_load(riid)
     # @info "Received QueryInterface: $guid"
     if guid == IID_IUNKNOWN || guid == IID_Typed_Event_Handler
@@ -133,17 +124,17 @@ function RecievedCallback_QueryInterface(this::Ptr{IRecievedCallback}, riid::Ptr
     return reinterpret(HRESULT, E_NOINTERFACE)
 end
 
-function RecievedCallback_AddRef(this::Ptr{IRecievedCallback})::UInt32
+function RecievedCallback_AddRef(this::Ptr{IEventHandler})::UInt32
     # @info "Received AddRef"
     return 1
 end
 
-function RecievedCallback_Release(this::Ptr{IRecievedCallback})::UInt32
+function RecievedCallback_Release(this::Ptr{IEventHandler})::UInt32
     # @info "Received Release"
     return 1
 end
 
-function RecievedCallback_Invoke(this::Ptr{IRecievedCallback}, watcher::Ptr{IBluetoothLEAdvertisementWatcher}, eventArgs::Ptr{Cvoid})::HRESULT
+function RecievedCallback_Invoke(this::Ptr{IEventHandler}, watcher::Ptr{IBluetoothLEAdvertisementWatcher}, eventArgs::Ptr{Cvoid})::HRESULT
     # try
         # @info "Received Invoke"
         eventArgs = Ptr{IBluetoothLEAdvertisementReceivedEventArgs}(eventArgs)
@@ -169,19 +160,22 @@ function RecievedCallback_Invoke(this::Ptr{IRecievedCallback}, watcher::Ptr{IBlu
     return S_OK
 end
 
-vtbl = IRecievedCallbackVtbl()
-vtbl.QueryInterface = @cfunction(RecievedCallback_QueryInterface, HRESULT, (Ptr{IRecievedCallback}, Ptr{GUID}, Ptr{Ptr{Cvoid}}))
-vtbl.AddRef = @cfunction(RecievedCallback_AddRef, UInt32, (Ptr{IRecievedCallback},))
-vtbl.Release = @cfunction(RecievedCallback_Release, UInt32, (Ptr{IRecievedCallback},))
-vtbl.Invoke = @cfunction(RecievedCallback_Invoke, HRESULT, (Ptr{IRecievedCallback}, Ptr{IBluetoothLEAdvertisementWatcher}, Ptr{Cvoid})) 
-callback = pointer_from_objref(vtbl) |> Interface{IRecievedCallbackVtbl} |> JComObj{IRecievedCallback}
+handlerImp = IEventHandlerVtbl(
+    IUnknownVtbl(
+        @cfunction(RecievedCallback_QueryInterface, HRESULT, (Ptr{IEventHandler}, Ptr{GUID}, Ptr{Ptr{Cvoid}})),
+        @cfunction(RecievedCallback_AddRef, UInt32, (Ptr{IEventHandler},)),
+        @cfunction(RecievedCallback_Release, UInt32, (Ptr{IEventHandler},))
+    ),
+    @cfunction(RecievedCallback_Invoke, HRESULT, (Ptr{IEventHandler}, Ptr{IBluetoothLEAdvertisementWatcher}, Ptr{Cvoid}))
+) |> Ref
+handler = IEventHandler(pointer_from_objref(handlerImp)) |> Ref
 
 status = BluetoothLEAdvertisementWatcherStatus(0) |> Ref
 mode = BluetoothLEScanningMode(0) |> Ref
 token = UInt64(0) |> Ref
 
 put_ScanningMode(watcher[], Active) |> AssertSuccess
-add_Received(watcher[], callback[], token)
+add_Received(watcher[], handler, token)
 Start(watcher[]) |> AssertSuccess
 for i in 10:-1:1
     get_Status(watcher[], status) |> AssertSuccess
@@ -193,3 +187,9 @@ put_ScanningMode(watcher[], Passive) |> AssertSuccess
 Stop(watcher[]) |> AssertSuccess
 
 @info "Done"
+
+# TDOD
+# - GetGattServicesAsync
+# - GetGattCharacteristicsAsync
+# - GetGattDescriptorsAsync
+
