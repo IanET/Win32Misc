e83dcbb9d55360dca8022abe71c413bb23f1e950
IanET <1154506+IanET@users.noreply.github.com>
2026-04-13 23:01:52 -0700

    ...

--- Widgets/Client/UpdateTemplate.app..jl
+++ Widgets/Client/UpdateTemplate.app..jl
@@ -1,7 +1,7 @@
 using JSON3, Dates
 
 const PIPE_NAME = "\\\\.\\pipe\\HelloWidgetProvider.d94hev71b6gse"
-const nowstr = Dates.format(Dates.now(), "HH:MM:SS")
+const nowstr = Dates.format(Dates.now(), "I:MM:SS p")
 const template = """
     {
         "type":"AdaptiveCard",
@@ -20,11 +20,6 @@ const template = """
 # Escape the template JSON as a string value inside the outer JSON message
 escaped = replace(template, "\\" => "\\\\", "\"" => "\\\"", "\n" => "")
 msg = """{"template":"$escaped","data":"{}"}"""
-# @info "Message" msg
-# json = JSON3.read(msg)
-# template_parsed = JSON3.read(json["template"])
-# data_parsed = JSON3.read(json["data"])
-# @info "Parsed" JSON3.write(json) template_parsed data_parsed
 
 pipe = open(PIPE_NAME, "w")
 println(pipe, msg)
